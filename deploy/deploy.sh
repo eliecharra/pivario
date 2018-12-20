675baa340192aef9b5857faf4cb109bd763550e7
Elie CHARRA <elie.charra@kibatic.com>
2018-12-20 20:09:52 +0100

    Fix travis deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,8 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
 eval $(ssh-agent -s)
-ssh-add deploy_key
+ssh-add deploy/deploy_key
 
+git checkout master
 git remote add balena gh_eliecharra@git.balena-cloud.com:gh_eliecharra/pivario0.git
 git push balena
