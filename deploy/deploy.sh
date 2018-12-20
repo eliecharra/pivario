bcf4fbdd3ae32bcf5ef9075d0cc38e51da8f3e40
Elie CHARRA <elie.charra@kibatic.com>
2018-12-20 20:15:23 +0100

    Try to fix travis deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-##
-## Create the SSH directory and give it the right permissions
-##
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-
-eval $(ssh-agent -s)
-ssh-add deploy/deploy_key
-
 git checkout master
 git remote add balena gh_eliecharra@git.balena-cloud.com:gh_eliecharra/pivario0.git
 git push balena
